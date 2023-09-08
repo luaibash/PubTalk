bd5f5565c101d51e8b20982ca0af57ddbc76c27c
Luai Bashar <luai.hawa@gmail.com>
2023-09-08 16:03:00 -0400

    Added team and article page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
+import Team from './pages/Team';
+import Article from './pages/Article';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './App.css';
@@ -12,6 +14,8 @@ function App() {
         <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/team' element={<Team/>} />
+            <Route path='/Article' element={<Article/>} />
           </Routes>
         <Footer/>
       </Router>
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Team = () => {
     return (
         <div>
-            Home Page
+            Team Page
         </div>
     );
 }
 
-export default Home;
+export default Team;
