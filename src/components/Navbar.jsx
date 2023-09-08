0b71023b96497f339aa41b56d850d8db4dd96a18
Luai Bashar <luai.hawa@gmail.com>
2023-09-08 16:50:29 -0400

    Added name and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import Logo from '../assets/logo/BlackLogo.png';
+import Name from '../assets/name/ArticleName.png';
 
 const Navbar = () => {
     return (
         <div className='NavbarContainer'>
             <div className='Navbar'>
-                Navbar
+                <div className='NavbarLogoNameContainer'>
+                    <img src={Logo} alt="Article Logo" className='NavbarLogo'/>
+                    <img src={Name} alt="Article Name" className='NavbarName'/>
+                </div>
             </div>
         </div>
     )
