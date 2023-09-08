5ed1264fbcefa559edd4c9ab3c8f108f2c3ee8da
Luai Bashar <luai.hawa@gmail.com>
2023-09-08 18:10:57 -0400

    Added pages for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useLocation, useNavigate} from "react-router-dom";
 import '../styles/Navbar.css';
 import Logo from '../assets/logo/BlackLogo.png';
 import Name from '../assets/name/ArticleName.png';
@@ -7,13 +8,38 @@ const Navbar = () => {
     return (
         <div className='NavbarContainer'>
             <div className='Navbar'>
-                <div className='NavbarLogoNameContainer'>
+                <div className='NavbarLeft'>
                     <img src={Logo} alt="Article Logo" className='NavbarLogo'/>
                     <img src={Name} alt="Article Name" className='NavbarName'/>
+                </div>
+                <div className='NavbarRight'>
+                    <NavbarPageItem pageName="Home"/>
+                    <NavbarPageItem pageName="Team"/>
+                    <NavbarPageItem pageName="Contact"/>
                 </div>
             </div>
         </div>
     )
 }
+
+const NavbarPageItem = ({ pageName }) => {
+    const destination = "/" + pageName.toLowerCase();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const goToLocation = (location) => {
+        navigate(location);
+        window.scrollTo(0,0);
+    };
+
+    return (
+        <div
+            className='NavbarPage'
+            id={location.pathname === destination ? "NavbarPageActive" : "NavbarPageInactive"}
+            onClick={() => goToLocation(destination)}
+        >
+            {pageName}
+        </div>
+    );
+};
 
 export default Navbar;
