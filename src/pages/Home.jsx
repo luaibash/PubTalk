aa3798d3bc50dea5294789cd5a44cad4f17cd6e6
Luai Bashar <luai.hawa@gmail.com>
2023-09-09 15:12:43 -0400

    Added all starting panels

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     return (
         <div>
             <PanelOne/>
+            <PanelTwo/>
+            <PanelThree/>
+            <PanelFour/>
         </div>
     );
 }
@@ -13,6 +16,30 @@ const PanelOne = () => {
     return (
         <div className='PanelOne'>
             Panel One
+        </div>
+    );
+}
+
+const PanelTwo = () => {
+    return (
+        <div className='PanelTwo'>
+            Panel Two
+        </div>
+    );
+}
+
+const PanelThree = () => {
+    return (
+        <div className='PanelThree'>
+            Panel Three
+        </div>
+    );
+}
+
+const PanelFour = () => {
+    return (
+        <div className='PanelFour'>
+            Panel Four
         </div>
     );
 }
