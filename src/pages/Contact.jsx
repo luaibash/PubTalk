24d5a5c717339bb03310c576b159c70ed971e20c
Luai Bashar <luai.hawa@gmail.com>
2023-09-12 20:33:08 -0400

    Added text to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,17 @@ import '../styles/Contact.css';
 const Contact = () => {
     return (
         <div className='ContactPanel'>
-            Contact
+            <div className='Title'>
+                Contact Us.
+            </div>
+            <div className='Subtext' id='ContactSubtext'>
+                Whether it&#39;s a new article topic, or you would, we would love to
+                hear from you. Enter your contact info below and your message,
+                and we&#39;ll get back to you shortly. 
+            </div>
+            <div className='ContactBox'>
+
+            </div>
         </div>
     );
 }
