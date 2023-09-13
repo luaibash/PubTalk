defe6c12ad3efb7acbe80b5cdf278ed89232946a
Luai Bashar <luai.hawa@gmail.com>
2023-09-12 23:53:38 -0400

    Fixed subtext

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                 Contact Us.
             </div>
             <div className='Subtext' id='ContactSubtext'>
-                Whether it&#39;s a new article topic, or you would, we would love to
-                hear from you. Enter your contact info below and your message,
-                and we&#39;ll get back to you shortly. 
+                Whether it&#39;s a new article topic, or you would like to give us advice,
+                we would love to hear from you. Enter your contact info below and
+                your message, and we&#39;ll get back to you shortly. 
             </div>
             <div className='ContactBoxContainer'>
                 <div className='ContactBox'>
