063c53e2dd61d4fe541f7d669f58a7914540e4d4
Luai Bashar <luai.hawa@gmail.com>
2023-09-12 23:26:38 -0400

    Added first row of contact box

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,8 +12,26 @@ const Contact = () => {
                 hear from you. Enter your contact info below and your message,
                 and we&#39;ll get back to you shortly. 
             </div>
-            <div className='ContactBox'>
-
+            <div className='ContactBoxContainer'>
+                <div className='ContactBox'>
+                    <div className='ContactBoxTitle'>
+                        Talk with our team
+                    </div>
+                    <div className='ContactBoxRow'>
+                        <div className='ContactBoxInputContainer'>
+                            First Name
+                            <input type="text" placeholder='e.g. John' className='ContactBoxInput'/>
+                        </div>
+                        <div className='ContactBoxInputContainer'>
+                            Last Name
+                            <input type="text" placeholder='Smith' className='ContactBoxInput'/>
+                        </div>
+                        <div className='ContactBoxInputContainer'>
+                            Email
+                            <input type="text" placeholder='john@gmail.com' className='ContactBoxInput'/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
