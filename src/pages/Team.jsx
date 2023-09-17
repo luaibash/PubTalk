76a985a78d9a22142e8a09ab579b3b3846f5e97c
Luai Bashar <luai.hawa@gmail.com>
2023-09-17 13:48:35 -0400

    Made animation for return to image merge

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -13,14 +13,25 @@ const Team = () => {
 
     const scroll = ({ left, right }) => {
         requestAnimationFrame(() => {
+            // Retrieve current margin value
             const computedStyle = window.getComputedStyle(memberRef.current);
             var marginLeft = computedStyle.getPropertyValue('margin-left');
             marginLeft = parseInt(marginLeft, 10);
 
-            if (left) marginLeft -= 350;
-            else marginLeft += 350;
-
+            // Add current shift in margin based on direction, and apply it
+            if (left) marginLeft += 350;
+            else if (right) marginLeft -= 350;
             memberRef.current.style.marginLeft = `${marginLeft}px`;
+
+            // Wait until shift has completed. Check if threshold reached, then return to middle.
+            setTimeout(() => {
+                if (marginLeft <= -3675 || marginLeft >= -175) {
+                    memberRef.current.style.transition = 'none';
+                    memberRef.current.style.marginLeft = '-1925px';
+                };
+            }, 500);
+
+            memberRef.current.style.transition = 'margin-left 0.5s ease';
         });
     }
 
@@ -38,6 +49,16 @@ const Team = () => {
             </div>
             <div className='MembersContainer'>
                 <div className='Members' ref={memberRef}>
+                    <Member name='Alex S.' role='Head Article Designer' colour='#D661FF'/>
+                    <Member name='Luai Bashar' role ='Head Software Developer' headshot={Luai} link='https://www.linkedin.com/in/luaibashar' colour='#FF6161'/>
+                    <Member name='Ivan Manca' role='Head Article Designer' headshot={Ivan} link='https://www.linkedin.com/in/ivan-manca-b27b17260' colour='#FCFF72'/>
+                    <Member name='Owen Skanes' role='Head Article Designer' headshot={Owen} colour='#72FF80'/>
+                    <Member name='Gabriel Hernandez' role='Student of the game' headshot={Gabe} colour='#7299FF'/>
+                    <Member name='Alex S.' role='Head Article Designer' colour='#D661FF'/>
+                    <Member name='Luai Bashar' role ='Head Software Developer' headshot={Luai} link='https://www.linkedin.com/in/luaibashar' colour='#FF6161'/>
+                    <Member name='Ivan Manca' role='Head Article Designer' headshot={Ivan} link='https://www.linkedin.com/in/ivan-manca-b27b17260' colour='#FCFF72'/>
+                    <Member name='Owen Skanes' role='Head Article Designer' headshot={Owen} colour='#72FF80'/>
+                    <Member name='Gabriel Hernandez' role='Student of the game' headshot={Gabe} colour='#7299FF'/>
                     <Member name='Alex S.' role='Head Article Designer' colour='#D661FF'/>
                     <Member name='Luai Bashar' role ='Head Software Developer' headshot={Luai} link='https://www.linkedin.com/in/luaibashar' colour='#FF6161'/>
                     <Member name='Ivan Manca' role='Head Article Designer' headshot={Ivan} link='https://www.linkedin.com/in/ivan-manca-b27b17260' colour='#FCFF72'/>
