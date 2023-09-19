067d8ac7285613b5b014d1ec0349c3732f958c9b
Luai Bashar <luai.hawa@gmail.com>
2023-09-19 00:17:13 -0400

    Added title and search bar

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../styles/Articles.css';
+import Search from '../assets/articles/Search.png';
 
 const Articles = () => {
     return (
         <div  className='ArticlePanel'>
-            Article Page
+            <div className='Title' id='ArticleTitle'>
+                Explore Our Articles: Find Your Desired
+                Topics Here
+            </div>
+            <div className='SearchContainer'>
+                <input type="text" placeholder='Search any topic (e.g. Politics) ' className='Search'/>
+            </div>
         </div>
     );
 }
