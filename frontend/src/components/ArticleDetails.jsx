ffb03692f9236e35f4f270b11082e274e17101ce
Luai Bashar <luai.hawa@gmail.com>
2023-12-22 11:38:57 -0500

    differentiated large and small article previews

--- frontend/src/components/ArticleDetails.jsx
+++ frontend/src/components/ArticleDetails.jsx
@@ -6,12 +6,12 @@ const ArticleDetails = ({article, large}) => {
     return(
         <div className='articlePreview'>
             <img src={Test} alt="Test" className='thumbnail'/>
-            <div className='articleContent'>
-                <div className='articleTitle'>{article.title}</div>
+            <div className='articleContent' id={large ? 'articleContentLarge' : ''}>
+                <div className='articleTitle' id={large ? 'articleTitleLarge' : ''}>{article.title}</div>
                 {large && <div className='articleDescription'>{article.description}</div>}
                 <div className='articleBottomContainer'>
-                    <div className='articleAuthor'>{article.author}</div>
-                    <div className='articleDuration'>{article.duration} min</div>
+                    <div className='articleAuthor' id={large ? 'articleDetailsLarge' : ''}>{article.author}</div>
+                    <div className='articleDuration' id={large ? 'articleDetailsLarge' : ''}>{article.duration} min</div>
                 </div>
             </div>
         </div>
