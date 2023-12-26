c9182589f29132fe1c5d511eefe864bacb23e936
Luai Bashar <luai.hawa@gmail.com>
2023-12-25 20:17:02 -0500

    Added all content to error page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import RedirectButton from '../components/RedirectButton';
 import '../styles/NotFound.css';
+import ReadingError from '../assets/Error/ReadingError.png';
 
 const NotFound = () => {
   return (
-    <div>
-      <h2 className='NotFoundText'>404 Not Found</h2>
-      <p className='NotFoundText'>Sorry, the page you are looking for does not exist.</p>
+    <div className='NotFoundPanel'>
+        <div className='ErrorDetailsContainer'>
+            <div className='ErrorNumber'>
+                Oops,
+                <br/>
+                nothing here...
+            </div>
+            <div className='NotFoundText'>
+                Sorry, that article does not exist, but we can help you find other great articles to read.
+            </div>
+            <div className='ErrorButton'>
+                <RedirectButton title={"Go Home"} destination={"/"}/>
+            </div>
+        </div>
+        <img src={ReadingError} alt="404" className='ReadingError' />
     </div>
   );
 };
