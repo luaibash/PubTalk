c1a5017ee3e08ef1fb6605cb9b3db7c3d60c4712
Luai Bashar <luai.hawa@gmail.com>
2024-01-09 12:02:19 -0500

    added equal sign to remove warning

--- frontend/src/pages/Articles.jsx
+++ frontend/src/pages/Articles.jsx
@@ -180,7 +180,7 @@ const ArticleGenres = () => {
                             {articles && articles[(currentPage - 1) * 4 + index] && <ArticleDetails article={articles[(currentPage - 1) * 4 + index]} genreArticle={true} articlesPerPage={4}/>}
                         </div>
                     ))}
-                    {articles && articles.length == 0 &&
+                    {articles && articles.length === 0 &&
                     <div className='GenreEmpty'>
                         There are currently no articles under this genre.
                     </div>
