6ec461ecf3f20cb0b77d4333a029c90ecb4293db
luaibash <luai.hawa@gmail.com>
2024-05-22 19:21:06 -0400

    Removed testing code for author article, switched it back to fetch author articles

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -193,9 +193,8 @@ const AuthorArticles = ({ article }) => {
     useEffect(() => {
         const fetchArticles = async () => {
             // Fetches the API and finds article using its id, providing a limit on articles to grab, and to exclude the current article being viewed
-            // const articleToExclude = JSON.stringify(article);
-            // const response = await fetch(`/api/articles/author/${article.author}?limit=4&excludeArticle=${articleToExclude}`);
-            const response = await fetch(`/api/articles?limit=4`);
+            const articleToExclude = JSON.stringify(article);
+            const response = await fetch(`/api/articles/author/${article.author}?limit=4&excludeArticle=${articleToExclude}`);
             const json = await response.json();
 
             if (response.ok) setArticles(json);
