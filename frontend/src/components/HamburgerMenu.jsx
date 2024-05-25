ae4e22413af09076a593d2c3dd818ce1c3cb6b10
luaibash <luai.hawa@gmail.com>
2024-05-25 19:54:17 -0400

    Fixed dark overlay transition

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -5,7 +5,7 @@ import '../styles/components/HamburgerMenu.css';
 const HamburgerMenu = ({showHamburgerMenu, updateHamburgerMenu}) => {
     return (
         <div className='OverlayAndHamburgerMenu'>
-            <div className='DarkOverlay' id={showHamburgerMenu ? 'DarkOverlayActive' : 'DarkOverlayInactive'}/>
+            <div className='DarkOverlay' id={showHamburgerMenu ? 'DarkOverlayActive' : ''}/>
             <div className='HamburgerMenuContainer' id={showHamburgerMenu ? 'HamburgerMenuActive' : 'HamburgerMenuInactive'} onClick={() => updateHamburgerMenu()}>
                 <div className='HamburgerMenuTop'>
 
