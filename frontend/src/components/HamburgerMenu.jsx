47f2be94259fafa258ef6fe36be40dcdeb389841
luaibash <luai.hawa@gmail.com>
2024-05-25 20:41:19 -0400

    Added CloseIcon to hamburger menu

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -1,4 +1,5 @@
 import {React} from 'react';
+import CloseIcon from '../assets/hamburgerMenu/CloseIcon.png';
 import '../styles/components/HamburgerMenu.css';
 
 // The menu that is on the navbar when the screen is too small, that when clicked, expands to show all pages to redirect to
@@ -6,9 +7,9 @@ const HamburgerMenu = ({showHamburgerMenu, updateHamburgerMenu}) => {
     return (
         <div className='OverlayAndHamburgerMenu'>
             <div className='DarkOverlay' id={showHamburgerMenu ? 'DarkOverlayActive' : ''}/>
-            <div className='HamburgerMenuContainer' id={showHamburgerMenu ? 'HamburgerMenuActive' : 'HamburgerMenuInactive'} onClick={() => updateHamburgerMenu()}>
+            <div className='HamburgerMenuContainer' id={showHamburgerMenu ? 'HamburgerMenuActive' : 'HamburgerMenuInactive'}>
                 <div className='HamburgerMenuTop'>
-
+                    <img src={CloseIcon} alt="Close Icon" className='CloseIcon' onClick={() => updateHamburgerMenu()}/>
                 </div>
             </div>
         </div>
